33edcbddf64219ed562d59358fb8ecee901410aa
KAKERU <43anillo@gmail.com>
2025-02-10 23:07:53 +0000

    header,menu,main完成

--- others/js/script.js
+++ others/js/script.js
@@ -1,16 +1,6 @@
 //ページロード時に発火
 document.addEventListener('DOMContentLoaded', loadfinish);
 function loadfinish(){
-    fetch('../others/common/menu.html')
-    .then(response => {
-      if (!response.ok) {
-       throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then(html => {
-      document.getElementById('menu_inport').innerHTML = html;
-
       //今いるページのメニューの背景をそろえてる。
       var filepath = document.location.pathname;
       filepath = filepath.replaceAll("/","");
@@ -18,8 +8,4 @@ function loadfinish(){
       var selected = document.getElementById(filepath);
       var selectedchild = selected.children;
       selectedchild[0].style.backgroundColor = "#7a6c52";
-    })
-    .catch(error => {
-      console.error('Error loading the menu:', error);
-    })
 }
