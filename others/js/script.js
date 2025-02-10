4712cec422de224314ca69c3e68283b3bb012a2a
KAKERU <43anillo@gmail.com>
2025-02-10 22:32:00 +0000

    a

---
+++ others/js/script.js
@@ -0,0 +1,26 @@
+//ページロード時に発火
+document.addEventListener('DOMContentLoaded', loadfinish);
+function loadfinish(){
+    fetch('../others/common/menu.html')
+    .then(response => {
+      if (!response.ok) {
+       throw new Error('Network response was not ok');
+      }
+      return response.text();
+    })
+    .then(html => {
+      document.getElementById('menu_inport').innerHTML = html;
+
+      //今いるページのメニューの背景をそろえてる。
+      var filepath = document.location.pathname;
+      filepath = filepath.replaceAll("/","");
+      filepath = "m_u_" + filepath;
+      var selected = document.getElementById(filepath);
+      console.log(selected);
+      var selectedchild = selected.children;
+      selectedchild[0].style.backgroundColor = "#7a6c52";
+    })
+    .catch(error => {
+      console.error('Error loading the menu:', error);
+    });
+}
